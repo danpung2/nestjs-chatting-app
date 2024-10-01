f298df0c70cccf08265a876d75d227bb461fac5b
danpung2 <danpung_2@naver.com>
2024-10-01 22:52:45 +0900

    add: style

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,6 @@ const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
-const currentUserElement = getElementById('current_user');
 const currentUsernameElement = getElementById('current_username');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
@@ -11,8 +10,8 @@ socket.on('user_connected', (username) => {
   drawNewChat(`${username} connected!`);
 });
 
-socket.on("user_disconnected", (username) => {
-    drawNewChat(`${username} disconnected!`);
+socket.on('user_disconnected', (username) => {
+  drawNewChat(`${username} disconnected!`);
 });
 
 socket.on('new_chat', (data) => {
@@ -26,18 +25,29 @@ const handleSubmit = (event) => {
 
   if (inputValue !== '') {
     socket.emit('submit_chat', inputValue);
-    drawNewChat(`me: ${inputValue}`);
+    drawNewChat(`me: ${inputValue}`, true);
     event.target.elements[0].value = '';
   }
 };
 
-const drawNewChat = (message) => {
+const drawNewChat = (message, isMe = false) => {
   const wrapperChatBox = document.createElement('div');
-  const chatBox = `
-        <div>
-            ${message}
-        </div>
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
     `;
+  else
+    chatBox = `
+    <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+
   wrapperChatBox.innerHTML = chatBox;
   chattingBoxElement.append(wrapperChatBox);
 };
